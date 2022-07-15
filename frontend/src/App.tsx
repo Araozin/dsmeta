2b5c732c50bd3a7b5182412e7d142f33e2527c3b
Arão Vitor <araodeveloper@gmail.com>
2022-07-15 15:45:04 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/header"
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NotificationButton from "./components/NotificationButton"
 import SalesCard from "./components/SalesCard"
 function App(): JSX.Element {
 
   return (
     <>
+    <ToastContainer/>
       <Header></Header>
       <main>
         <section id="sales">
